ac4a54485625854a25a50641de3f6b4fb6392fb8
jessica <jessica.omondi@student.moringaschool>
2023-10-20 07:53:32 +0300

    assingment done

--- challenge1.js
+++ challenge1.js
@@ -1,30 +1,31 @@
-//function that generates the grade of a student according to their marks they score
-function generateStudentGrade(mark){
-    let grade;
-//we are converting the datatype to float
-        parseFloat(mark);
-// Check if the input is a valid number
-        if (isNaN(mark) || mark < 0 || mark > 100) {
-            alert("Invalid input. Please enter a number between 0 and 100.");
-            return;
-        }
-// Determine the grade based on the marks scored
-        if(mark > 79){
-            grade = 'A';
-        }else if(mark >= 60 && mark <= 79){
-            grade = 'B';
-        }else if(mark >= 50 && mark <= 59){
-            grade = 'C';
-        }else if(mark >= 40 && mark <= 49){
-            grade = 'D';
-        }else{
-            grade = 'E';
-        }
-        console.log(`The Student's grade is: ${grade}`)
-  
-    return mark
-    
-}
-//calling the function
+function calculateGrade(mark) {
+    if (mark > 79) {
+      return 'A';
+    } else if (mark >= 60) {
+      return 'B';
+    } else if (mark >= 50) {
+      return 'C';
+    } else if (mark >= 40) {
+      return 'D';
+    } else {
+      return 'E';
+    }
+  }
 
-generateStudentGrade(80)
+  function studentGradeGenerator() {
+    const mark = parseFloat(prompt('Enter the student mark (0-100):'));
+
+    if (isNaN(mark) || mark < 0 || mark > 100) {
+      return 'Invalid input. Please enter a valid mark between 0 and 100.';
+    }
+
+    const grade = calculateGrade(mark);
+    return `The student's grade is: ${grade}`;
+  }
+
+
+  const result = studentGradeGenerator();
+  console.log(result);
+
+
+  
